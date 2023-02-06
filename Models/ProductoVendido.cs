44ceac9e0e7b3a9eaf00f191e7a54875052d1005
Agustin Ciano <98432897+AgustinCiano@users.noreply.github.com>
2023-02-06 10:52:11 -0300

    Add files via upload

---
+++ Models/ProductoVendido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraEntrega.Models
+{
+    internal class ProductoVendido
+    {
+        private long id;
+        private int Stock;
+        private long idProducto;
+        private long idVenta;
+
+        public long Id { get => id; set => id = value; }
+        public int Stock1 { get => Stock; set => Stock = value; }
+        public long IdProducto { get => idProducto; set => idProducto = value; }
+        public long IdVenta { get => idVenta; set => idVenta = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using PrimeraEntrega.Handlers;
+using PrimeraEntrega.Models;
+
+namespace PrimeraEntrega
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //*********** Escribir una funcion que me permita obtener todos los productos de la base
+            List<Producto> productos = ProductoHandler.ObtenerProductos();
+
+
+            //********* Escribir una funcion que me permita obtener un producto dado las descripciones
+            //Producto producto = ProductoHandler.ObtenerProducto("Medias");
+
+
+
+            //******* Escribir una funcion que me permita crear un producto
+            //Producto productoAInsertar = new Producto();
+            //productoAInsertar.Descripciones = "Pantalon Jean Negro";
+            //productoAInsertar.Costo = 9000;
+            //productoAInsertar.PrecioVenta = 12000;
+            //productoAInsertar.Stock = 99;
+            //productoAInsertar.IdUsuario = 2;
+
+            //if (ProductoHandler.InsertarProducto(productoAInsertar) >= 1)
+            //{
+            //    Console.WriteLine("Producto Insertado");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No se pudo insertar el producto");
+            //}
+
+
+
+        }
+    }
+}
